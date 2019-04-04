f02fa9775b4a438abe475ef1a69135eb7aeed831
jules <julescordonnier@hotmail.fr>
2019-04-04 11:33:19 +0200

    Score graphs

--- app/src/main/java/com/example/texway/Fragment/ScoreFragment.java
+++ app/src/main/java/com/example/texway/Fragment/ScoreFragment.java
@@ -1,15 +1,36 @@
 package com.example.texway.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.example.texway.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ScoreFragment extends Fragment {
 
+    private PieChart mChart;
+    private float[] yData = { 5, 20, 45, 30 };
+    private String[] xData = { "Excellent", "Bon", "Mediocre","Mauvais"};
 
     public ScoreFragment() {
         // Required empty public constructor
@@ -26,7 +47,86 @@ public class ScoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_score, container, false);
+
+        mChart = rootView.findViewById(R.id.piechart);
+
+        // configure pie chart
+        mChart.setUsePercentValues(true);
+        mChart.setDescription(new Description());
+
+        // enable hole and configure
+        mChart.setDrawHoleEnabled(true);
+        //mChart.setHoleColorTransparent(true);
+        mChart.setHoleRadius(7);
+        mChart.setTransparentCircleRadius(10);
+
+        // enable rotation of the chart by touch
+        mChart.setRotationAngle(0);
+        mChart.setRotationEnabled(true);
+        mChart.getDescription().setEnabled(false);
+        // add data
+        addData();
+
+        // customize legends
+        Legend l = mChart.getLegend();
+
+        l.setEnabled(true);
+
+        //l.se(LegendPosition.RIGHT_OF_CHART);
+        l.setXEntrySpace(7);
+        l.setYEntrySpace(5);
+
+
         return rootView;
     }
+
+    private void addData() {
+        List<PieEntry> yVals1 = new ArrayList<PieEntry>();
+
+        for (int i = 0; i < yData.length; i++)
+            yVals1.add(new PieEntry(yData[i], xData[i]));
+
+
+        // create pie data set
+        PieDataSet dataSet = new PieDataSet(yVals1,"");
+        dataSet.setSliceSpace(3);
+        dataSet.setSelectionShift(5);
+
+        // add many colors
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        // instantiate pie data object now
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.GRAY);
+
+        mChart.setData(data);
+
+        // undo all highlights
+        mChart.highlightValues(null);
+
+        // update pie chart
+        mChart.invalidate();
+    }
+
 
 }
