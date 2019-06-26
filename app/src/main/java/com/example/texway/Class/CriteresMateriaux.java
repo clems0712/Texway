9ddb2c660d2e7e3fab373364eb4610e280616f6a
icoffi <zakzakdu92@hotmail.fr>
2019-06-26 22:02:41 +0200

    Materiaux.json Class qui récupère la composition et retourne la moyenne Class CriteresMateriaux

---
+++ app/src/main/java/com/example/texway/Class/CriteresMateriaux.java
@@ -0,0 +1,36 @@
+package com.example.texway.Class;
+
+public class CriteresMateriaux {
+    String materiaux;
+    String origine;
+    String categorie;
+    String notation;
+    String famille;
+
+    public String getNotation() {return notation;}
+
+    public void setNotation(String notation) {this.notation = notation;}
+
+
+    public String getCategorie() {return categorie;}
+
+    public void setCategorie(String categorie) {this.categorie = categorie;}
+
+
+    public String getMateriaux() {return materiaux;}
+
+    public void setMateriaux(String materiaux) {this.materiaux = materiaux;}
+
+
+    public String getOrigine() {return origine;}
+
+    public void setOrigine(String origine) {this.origine = origine;}
+
+
+    public String getFamille() {
+        return famille;
+    }
+    public void setFamille(String famille) {
+        this.famille = famille;
+    }
+}
